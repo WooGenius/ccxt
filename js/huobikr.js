86a8fd6510c76738de60fb38b485f4ce3a19c015
wook <wook@hyperithm.com>
2019-09-23 21:08:22 +0900

    update huobikr api endpoint

--- js/huobikr.js
+++ js/huobikr.js
@@ -1,30 +1,30 @@
-'use strict';
+"use strict"
 
 // ---------------------------------------------------------------------------
 
-const huobipro = require ('./huobipro.js');
+const huobipro = require("./huobipro.js")
 
 // ---------------------------------------------------------------------------
 
 module.exports = class huobikr extends huobipro {
-    describe () {
-        return this.deepExtend (super.describe (), {
-            'id': 'huobikr',
-            'name': 'Huobi Korea',
-            'hostname': 'api-cloud.huobi.co.kr',
-            'has': {
-                'CORS': false,
-            },
-            'urls': {
-                'logo': 'https://user-images.githubusercontent.com/1294454/27766569-15aa7b9a-5edd-11e7-9e7f-44791f4ee49c.jpg',
-                'api': {
-                    'public': 'https://api-cloud.huobi.co.kr',
-                    'private': 'https://api-cloud.huobi.co.kr',
-                    'market': 'https://api-cloud.huobi.co.kr',
-                },
-                'www': 'https://www.huobi.co.kr',
-                'doc': 'https://github.com/alphaex-api/BAPI_Docs_ko/wiki/REST-API-Reference',
-            },
-        });
-    }
-};
+  describe() {
+    return this.deepExtend(super.describe(), {
+      id: "huobikr",
+      name: "Huobi Korea",
+      hostname: "krapi.huobi.pro",
+      has: {
+        CORS: false
+      },
+      urls: {
+        logo: "https://user-images.githubusercontent.com/1294454/27766569-15aa7b9a-5edd-11e7-9e7f-44791f4ee49c.jpg",
+        api: {
+          public: "https://krapi.huobi.pro",
+          private: "https://krapi.huobi.pro",
+          market: "https://krapi.huobi.pro"
+        },
+        www: "https://www.huobi.co.kr",
+        doc: "https://github.com/alphaex-api/BAPI_Docs_ko/wiki/REST-API-Reference"
+      }
+    })
+  }
+}
